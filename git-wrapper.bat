f4dfa65b60b8d89d1ea4f37f3c84cab119898890
Maks <max.chodorowski@gmail.com>
2018-06-10 21:02:33 +0100

    Fix duplicated calls setting prompt

--- git-wrapper.bat
+++ git-wrapper.bat
@@ -76,8 +76,9 @@ call :listbranches "Select branch to checkout:" "Switching to branch" "git check
 
 :setprompt
 endlocal
-call %DYNAMICPROMPTSCRIPT%
 
+set GITBRANCH=
+for /f %%I in ('git.exe rev-parse --abbrev-ref HEAD 2^> NUL') do set GITBRANCH=%%I
 
 exit /B %ERRORLEVEL%
 
